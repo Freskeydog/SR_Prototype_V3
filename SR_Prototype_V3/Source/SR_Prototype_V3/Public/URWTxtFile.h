3ad71507094d5c590b7f356bf38fefb30048f7e7
Freskeydog <bendickmh@gmail.com>
2017-12-11 14:38:04 +0100

    URWTxtFile

---
+++ SR_Prototype_V3/Source/SR_Prototype_V3/Public/URWTxtFile.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "URWTxtFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SR_PROTOTYPE_V3_API UURWTxtFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
